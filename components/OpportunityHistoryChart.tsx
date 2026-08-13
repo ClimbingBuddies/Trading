226d6564be9e391653d92f57d7dc6ffe78a45383
climbingonboulders <climbingonboulders@gmail.com>
2026-08-13 19:10:24 +0800

    Make opportunity history chart palette aware

--- components/OpportunityHistoryChart.tsx
+++ components/OpportunityHistoryChart.tsx
@@ -16,14 +16,14 @@ export default function OpportunityHistoryChart({ data }: { data: OpportunityHis
     <div style={{ width: '100%', height: 320 }}>
       <ResponsiveContainer>
         <LineChart data={data} margin={{ top: 12, right: 18, bottom: 8, left: 0 }}>
-          <CartesianGrid strokeDasharray="3 3" vertical={false} />
+          <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="var(--chart-grid)" />
           <XAxis dataKey="date" tick={{ fontSize: 12 }} />
           <YAxis domain={[0, 100]} tick={{ fontSize: 12 }} width={36} />
           <Tooltip />
           <Legend />
-          <Line type="monotone" dataKey="opportunity" name="Opportunity" strokeWidth={3} dot={false} connectNulls />
-          <Line type="monotone" dataKey="structural" name="Structural" strokeWidth={2} dot={false} connectNulls />
-          <Line type="monotone" dataKey="technology" name="Technology Inflection" strokeWidth={2} dot={false} connectNulls />
+          <Line type="monotone" dataKey="opportunity" name="Opportunity" stroke="var(--chart-1)" strokeWidth={3} dot={false} connectNulls />
+          <Line type="monotone" dataKey="structural" name="Structural" stroke="var(--chart-3)" strokeWidth={2} dot={false} connectNulls />
+          <Line type="monotone" dataKey="technology" name="Technology Inflection" stroke="var(--chart-5)" strokeWidth={2} dot={false} connectNulls />
         </LineChart>
       </ResponsiveContainer>
     </div>
